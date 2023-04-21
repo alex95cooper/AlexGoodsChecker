bf2ef4f15e6b085df70f66a5fabba2b803787d82
alex95cooper <lex95bond@gmail.com>
2023-04-21 14:16:13 +0300

    Added class Product, added deserialization to project.

---
+++ AlexGoodsChecker/Product.cs
@@ -0,0 +1,8 @@
+namespace AlexGoodsChecker;
+
+public class Product
+{
+    public string Id { get; set; }  
+    public string MarketPlace { get; set; }
+    public string Url { get; set; }  
+}
--- AlexGoodsChecker/Program.cs
+++ AlexGoodsChecker/Program.cs
@@ -1,8 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Text.Json;
 using AlexGoodsChecker;
-string urlAddress = "https://www.amazon.com/Eric-Robertson-Christmas-Album-Vinyl/dp/B00J1197AI/ref=sr_1_167?crid=1JHK3PQ0HOSUG&keywords=metallica+black+album+vinyl&qid=1681921889&sprefix=metallica+black+%2Caps%2C225&sr=8-167";
-bool result = GetRequester.CheckProductAvailability(urlAddress);
-Console.WriteLine(result);
+
+List<Product> products = new();
+using (StreamReader reader = new StreamReader("appsettings.json"))  
+{  
+    string json = reader.ReadToEnd();  
+    products = JsonSerializer.Deserialize<List<Product>>(json);  
+}
+
+foreach (Product product in products)
+{
+    Console.WriteLine("--------------------------------------------------");
+    Console.WriteLine(product.Id);
+    Console.WriteLine(product.MarketPlace);
+    Console.WriteLine(product.Url);
+}
+Console.WriteLine("result");
 //File.WriteAllText("C:\\E\\Програмирование\\Amazon1.txt", result);
 Console.ReadKey();
+
+
