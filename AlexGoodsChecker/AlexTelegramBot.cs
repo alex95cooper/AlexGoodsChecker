ec2b01c6bcc14dab75e259a423c4cc479751f5ad
alex95cooper <lex95bond@gmail.com>
2023-04-28 12:40:56 +0300

    Deleted the ability to send messages to different chat.

--- AlexGoodsChecker/AlexTelegramBot.cs
+++ AlexGoodsChecker/AlexTelegramBot.cs
@@ -11,11 +11,12 @@ public class AlexTelegramBot
     private const string TelegramBotToken = "6065625282:AAEjYaSxmKLgq3GgWiwyNI2PnFFal8B6NPY";
 
     private const string AppActivityMessage =
-        "The application is working correctly. A total of {0} requests were made per day.";
+        "The application is working correctly. A total of {0} requests were made today.";
 
     private const string GoodsAvailabilityMessage = "Item with code {0} appeared in the store {1}.";
     private const string StartMessage = "Goods monitoring started";
     private const double MonitoringInterval = 500000;
+    private const long ChatId = -872421566;
 
     private readonly List<Product> _products;
     private readonly TelegramBotClient _botClient;
@@ -28,38 +29,18 @@ public class AlexTelegramBot
     {
         _products = Deserialize() ?? new List<Product>();
         _botClient = new TelegramBotClient(TelegramBotToken);
-        _botClient.StartReceiving(Update, Error);
         _chatIdList = new List<long>();
         _client = client;
     }
 
     public void Start()
     {
+        _botClient?.SendTextMessageAsync(ChatId, StartMessage);
         Timer timer = new(MonitoringInterval);
         timer.Elapsed += Start_Monitoring;
         timer.Start();
     }
-
-    private async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
-    {
-        var message = update?.Message ?? new Message();
-        if (message.Text != null && message.Text.Contains("/start"))
-        {
-            if (_chatIdList != null && !_chatIdList.Contains(message.Chat.Id))
-            {
-                _chatIdList.Add(message.Chat.Id);
-            }
-
-            await _botClient?.SendTextMessageAsync(message.Chat.Id, StartMessage, cancellationToken: token)!;
-        }
-    }
-
-    private static async Task Error(ITelegramBotClient botClient, Exception exception, CancellationToken token)
-    {
-        Console.WriteLine("Something wrong...");
-        await Task.Delay((int) MonitoringInterval, token);
-    }
-
+    
     private void Start_Monitoring(object sender, ElapsedEventArgs e)
     {
         if (_products != null)
@@ -69,12 +50,13 @@ public class AlexTelegramBot
             {
                 if (GetRequester.CheckProductAvailability(product, _client))
                 {
-                    MakeMessageSending(string.Format(GoodsAvailabilityMessage, product?.Id, product?.MarketPlace));
+                    _botClient?.SendTextMessageAsync(ChatId, string.Format(
+                        GoodsAvailabilityMessage, product?.Id, product?.MarketPlace));
                 }
             }
         }
 
-        MakeMessageSending(string.Format(AppActivityMessage, _count));
+        _botClient?.SendTextMessageAsync(ChatId, string.Format(AppActivityMessage, _count));
     }
 
     private static List<Product> Deserialize()
